d4a3ba2431dfb90b67f622968e2e4ea4de704040
OS <os@OSs-Mac-mini.local>
2025-05-15 13:07:34 -0500

    updates / dynamic posts editing I

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,47 +1,64 @@
 import React, { useState } from "react";
 import { MdOutlineMoreVert } from "react-icons/md";
 
+const Post = ({ post, users }) => {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
 
-const Post = ({ post }) => {
-    const [like, setLike] = useState(post.like);
-    const [isLiked, setIsLiked] = useState(false);
+  const handleLike = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
 
-    const handleLike = () => {
-        setLike(isLiked ? like - 1 : like + 1);
-        setIsLiked(!isLiked)
-    }
+  const user = users.find((u) => u.id === post.userId);
 
   return (
-    <div className='w-full rounded-md shadow-lg mt-[30px] mb-[30px] p-[10px]'>
-        <div className='p-[10px]'>
-            <div className='flex items-center justify-between'>
-                <div className='flex items-center'>
-                    <img src="/assets/profilePicture.jpg" alt='Profile Picture' className='w-[32px] h-[32px] rounded-full object-cover'/>
-                    <span className='font-bold ml-[10px] mr-[10px]'>Test Person</span>
-                    <span className='text-sm'>{post.date}</span>
-                </div>
-                <div>
-                    <MdOutlineMoreVert className='text-xl cursor-pointer'/>
-                </div>
-            </div>
+    <div className="w-full rounded-md shadow-lg mt-[30px] mb-[30px] p-[10px]">
+      <div className="p-[10px]">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <img
+              src={user.profilePicture}
+              alt="Profile Picture"
+              className="w-[32px] h-[32px] rounded-full object-cover"
+            />
+            <span className="font-bold ml-[10px] mr-[10px]">{user.username}</span>
+            <span className="text-sm">{post.date}</span>
+          </div>
+          <MdOutlineMoreVert className="text-xl cursor-pointer" />
         </div>
-        <div className='mt-[20px] mb-[20px]'>
-            <span>{post?.desc}</span>
-            <img src={`/assets/${post.photo}`} alt='Post Picture' className='mt-[20px] w-full object-contain' style={{maxHeight: "500px" }}/>
+      </div>
+      <div className="mt-[20px] mb-[20px]">
+        <span>{post?.desc}</span>
+        <img
+          src={post.photo}
+          alt="Post"
+          className="mt-[20px] w-full object-contain"
+          style={{ maxHeight: "500px" }}
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-[5px]">
+          <img
+            src="/assets/thumbsUp.png"
+            alt="thumbsUp"
+            className="w-[24px] h-[24px]"
+            onClick={handleLike}
+          />
+          <img
+            src="/assets/heart.png"
+            alt="heart"
+            className="w-[24px] h-[24px]"
+            onClick={handleLike}
+          />
+          <span className="text-sm">{like} Likes</span>
         </div>
-        <div className='flex items-center justify-between'>
-            <div className='flex items-center gap-[5px]'>
-                <img src="/assets/thumbsUp.png" alt='thumbsUp' className='w-[24px] h-[24px]' onClick={handleLike}/>
-                <img src="/assets/heart.png" alt='heart' className='w-[24px] h-[24px]' onClick={handleLike}/>
-                <span className='text-sm'>{like} Likes</span>
-
-            </div>
-            <div>
-                <span className='cursor-pointer border-b-[1px] border-slate-300 text-sm'>{post.comment} Comments</span>
-            </div>
-        </div>
+        <span className="cursor-pointer border-b-[1px] border-slate-300 text-sm">
+          {post.comment} Comments
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
