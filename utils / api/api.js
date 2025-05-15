0659225f52b4afd51afc3ff80e28df7c8dbace24
OS <os@OSs-Mac-mini.local>
2025-05-15 10:56:14 -0500

    updates / production push

--- utils / api/api.js
+++ utils / api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "https://mern-javascript-social-media-app.onrender.com/api/v1"
+    baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 export const getTimeLinePost = (username) =>
